2525dee25b73ccd34666ba37959fce2539ccd3b6
foticc <foticc@163.com>
2024-01-24 23:19:40 +0800

    Initial commit

---
+++ src/main/java/com/foticc/demo/graphql/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.foticc.demo.graphql.controller;
+
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GreetingController {
+
+
+    @QueryMapping
+    public String hello() {
+        return "Hello, world!";
+    }
+
+}
